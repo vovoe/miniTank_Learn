1eb0cc981cb54aba261bc57c9118ebfa2ad626c1
vovoe <125288774+vovoe@users.noreply.github.com>
2023-12-07 14:22:34 +0200

    addfile

---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void HandleDestruction();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float FireRange = 300.f;
+
+	class ATank *Tank;
+	FTimerHandle FireRateTimerHandle;
+	float FireRate = 2.f;
+
+	void CheckFireCondition();
+	bool InFireRange();
+};
